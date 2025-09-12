b262a459f3d05bc0552a19e0185791b732595e45
Syufan <zyf571635687@gmail.com>
2025-09-12 22:09:38 +0800

    fix: async to sync

--- server/Application/ITodoRepository.cs
+++ server/Application/ITodoRepository.cs
@@ -2,11 +2,11 @@ using Server.Domain;
 namespace Server.Application;
 public interface ITodoRepository
 {   // Read Operation - Get all todos
-    Task<IReadOnlyList<TodoItem>> ListAsync(CancellationToken ct);
+    IReadOnlyList<TodoItem> List();
 
     // Write Operation - Create new todo
-    Task<TodoItem> AddAsync(string title, CancellationToken ct);
+    TodoItem Add(string title);
 
     // Write Operation - Delete by ID, returns success/failure
-    Task<bool> DeleteAsync(int id, CancellationToken ct);
+    bool Delete(int id);
 }
