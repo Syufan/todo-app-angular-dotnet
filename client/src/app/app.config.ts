5d8e7201fafa5b57da8ae55c43f00ea3ae11ab41
Syufan <zyf571635687@gmail.com>
2025-09-14 00:23:10 +0800

    chore: add the config file

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,15 +4,27 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { correlationIdInterceptor } from './interceptors/correlation-id.interceptor';
+import { problemDetailsInterceptor } from './interceptors/problem-details.interceptor';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { API_BASE_URL } from './core/tokens';
+import { environment } from '../../../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        correlationIdInterceptor,
+        problemDetailsInterceptor,
+      ])
+    ),
+
+    { provide: API_BASE_URL, useValue: environment.apiBaseUrl },
   ],
 };
