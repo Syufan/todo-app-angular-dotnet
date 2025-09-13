250d6c4f03f20d4e3b36e7b00aa56a41aec5b579
Syufan <zyf571635687@gmail.com>
2025-09-14 00:01:35 +0800

    feat: add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TodosPage } from './pages/todos/todos.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TodosPage },
+  { path: '**', redirectTo: '' },
+];
