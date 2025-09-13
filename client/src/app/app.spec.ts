a12f1d151904a6c369c602219e0e739c0c3750d0
Syufan <zyf571635687@gmail.com>
2025-09-14 01:25:19 +0800

    fix: ESLint error

--- client/src/app/app.spec.ts
+++ client/src/app/app.spec.ts
@@ -1,4 +1,4 @@
-import { provideZonelessChangeDetection } from '@angular/core';
+// src/app/app.spec.ts
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
@@ -6,7 +6,6 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()],
     }).compileComponents();
   });
 
@@ -16,10 +15,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should render a router-outlet', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, client');
+    const el: HTMLElement = fixture.nativeElement;
+    expect(el.querySelector('router-outlet')).not.toBeNull();
   });
 });
