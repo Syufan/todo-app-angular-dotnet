dee61a33323ce3393dc94364355031df10a6ef53
Syufan <zyf571635687@gmail.com>
2025-09-13 23:08:14 +0800

    handle all backend interactions

---
+++ client/src/app/services/todo.service.ts
@@ -0,0 +1,19 @@
+// client/src/app/services/todo.service.ts
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_BASE_URL } from '../core/tokens';
+import { CreateTodoRequest, TodoItemDto } from '../models/todo.models';
+
+@Injectable({ providedIn: 'root' })
+export class TodoService {
+  private readonly http = inject(HttpClient);
+  private readonly base = inject(API_BASE_URL);
+
+  list()  { return this.http.get<TodoItemDto[]>(`${this.base}/todo`); }
+  add(title: string) {
+    const body: CreateTodoRequest = { title };
+    return this.http.post<TodoItemDto>(`${this.base}/todo`, body);
+  }
+  remove(id: number) { return this.http.delete<void>(`${this.base}/todo/${id}`); }
+}
