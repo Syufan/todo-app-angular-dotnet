05c544b3fa9f2717a44f278ca8b5e826dab6d4ba
Syufan <zyf571635687@gmail.com>
2025-09-13 13:54:13 +0800

    chore(frontend): fix coverage failure

---
+++ client/karma.conf.cjs
@@ -0,0 +1,29 @@
+/** @type {import('karma').Config} */
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      dir: require('path').join(__dirname, 'coverage/client'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly', file: 'lcov.info' },
+        { type: 'text-summary' }
+      ],
+      fixWebpackSourcePaths: true
+    },
+
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ]
+  });
+};
